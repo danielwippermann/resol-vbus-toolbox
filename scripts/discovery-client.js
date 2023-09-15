37b86d925864d8341deb003a6f772975b587d9a8
Daniel Wippermann <daniel.wippermann@gmail.com>
2023-09-15 09:41:17 +0200

    [discovery-client] Add support for IPv6.

--- scripts/discovery-client.js
+++ scripts/discovery-client.js
@@ -5,6 +5,7 @@
 /// ```
 /// ❯ bin/resol-vbus-toolbox --virtual --script scripts/discovery-client.js
 /// 192.168.2.101: My-DL2Plus
+/// fe80::xxxx:xxff:fexx:xxxx%en0: My-DL2Plus
 /// ```
 ///
 /// Example of a simulated device running within resol-vbus-toolbox:
@@ -16,15 +17,59 @@
 /// [scripts/webserver.js] - http://[::1]:3000/
 /// [scripts/webserver.js] - http://192.168.2.100:3000/
 /// 192.168.2.100: DL2-001E66000000
+/// fe80::1%lo0: DL2-001E66000000
 /// ```
+
+const os = require('os');
+
 
 await $.connect();
 
 const { TcpDataSourceProvider } = $.vbus;
 
-const devices = await TcpDataSourceProvider.discoverDevices();
+const promises = [
+    TcpDataSourceProvider.discoverDevices(),
+];
 
-for (const device of devices) {
+const ifaceMap = os.networkInterfaces();
+const ifaceNames = Object.getOwnPropertyNames(ifaceMap);
+
+for (const ifaceName of ifaceNames) {
+    const addresses = ifaceMap [ifaceName];
+    const hasIPv6 = addresses.some(address => address.family === 'IPv6');
+    if (hasIPv6) {
+        const promise = TcpDataSourceProvider.discoverDevices({
+            ipv6: true,
+            broadcastInterface: ifaceName,
+        });
+
+        promises.push(promise);
+    }
+}
+
+const allDevices = [];
+for (const promise of promises) {
+    const devices = await promise;
+    for (const device of devices) {
+        allDevices.push(device);
+    }
+}
+
+allDevices.sort((l, r) => {
+    if (l.name < r.name) {
+        return -1;
+    } else if (l.name > r.name) {
+        return 1;
+    } else if (l.__address__ < r.__address__) {
+        return -1;
+    } else if (l.__address__ > r.__address__) {
+        return 1;
+    } else {
+        return 0;
+    }
+});
+
+for (const device of allDevices) {
     console.log(`${device.__address__}: ${device.name}`);
 }
 
